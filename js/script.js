6105a246949561a9f82fb6584ae6a2e1c44a22d0
Dobroslawa Toranska <dobroslawa.toranska@gmail.com>
2020-07-01 21:40:48 +0100

    Zmienne i logika dla nozyc papieru i kamienia

--- js/script.js
+++ js/script.js
@@ -2,6 +2,31 @@ var computerMove;
 var playerMove;
 computerMove = 'kamień';
 playerMove = 'papier';
+var randomNumber;
+
 
 printMessage('Zagrałem ' + computerMove + '! Jeśli Twój ruch to papier, to wygrywasz!');
 printMessage('Zagrałem ' + playerMove + '! Jeśli Twój ruch to nozyce, to wygrywasz!');
+printMessage('Wylosowana liczba to: ' + randomNumber);
+randomNumber = Math.floor(Math.random() * 3 + 1);
+console.log('wylosowana liczba to: ' + randomNumber);
+if (randomNumber == '1') {
+  computerMove = 'kamień';
+} else {
+  computerMove = 'nieznany ruch';
+}
+printMessage('Mój ruch: ' + computerMove);
+
+if (randomNumber == '2') {
+    computerMove = 'papier';
+  } else {
+    computerMove = 'nieznany ruch';
+  }
+printMessage('Mój ruch: ' + computerMove);
+
+  if (randomNumber == '3') {
+    computerMove = 'nożyce';
+  } else {
+    computerMove = 'nieznany ruch';
+  }
+printMessage('Mój ruch: ' + computerMove);
