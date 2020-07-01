285c9b4bf461611e7f64a8e8c710950696153978
Dobroslawa Toranska <dobroslawa.toranska@gmail.com>
2020-07-01 21:12:38 +0100

    add computerMove playerMove zmienne

--- js/functions.js
+++ js/functions.js
@@ -4,6 +4,9 @@ function printMessage(msg){
 	document.getElementById('messages').appendChild(div);
 }
 
+printMessage('Zagrałem ' + 'kamień' + '! Jeśli Twój ruch to papier, to wygrywasz!');
+printMessage('Zagrałem ' + 'papier' + '! Jeśli Twój ruch to nozyce, to wygrywasz!');
+
 function clearMessages(){
 	document.getElementById('messages').innerHTML = '';
 }
---
+++ js/script.js
@@ -0,0 +1,7 @@
+var computerMove;
+var playerMove;
+computerMove = 'kamień';
+playerMove = 'papier';
+
+printMessage('Zagrałem ' + computerMove + '! Jeśli Twój ruch to papier, to wygrywasz!');
+printMessage('Zagrałem ' + playerMove + '! Jeśli Twój ruch to nozyce, to wygrywasz!');
