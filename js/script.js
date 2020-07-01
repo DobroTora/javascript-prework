abd915127706c069836aa022d275516df36dc640
Dobroslawa Toranska <dobroslawa.toranska@gmail.com>
2020-07-01 22:24:13 +0100

    Logika ruchow gracza

--- js/script.js
+++ js/script.js
@@ -1,9 +1,9 @@
 var computerMove;
 var playerMove;
+var randomNumber;
+var playerMove, playerInput;
 computerMove = 'kamień';
 playerMove = 'papier';
-var randomNumber;
-
 
 printMessage('Zagrałem ' + computerMove + '! Jeśli Twój ruch to papier, to wygrywasz!');
 printMessage('Zagrałem ' + playerMove + '! Jeśli Twój ruch to nozyce, to wygrywasz!');
@@ -30,3 +30,24 @@ printMessage('Mój ruch: ' + computerMove);
     computerMove = 'nieznany ruch';
   }
 printMessage('Mój ruch: ' + computerMove);
+
+playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.');
+console.log('Wpisana odpowiedź to: ' + playerInput);
+if (playerInput == '1') {
+  playerMove = 'kamień';
+} else {
+    printMessage('nieznany ruch' + playerMove);
+}
+printMessage('Twój ruch: ' + playerMove);
+if (playerInput == '2') {
+    playerMove = 'papier';
+  } else {
+    printMessage('nieznany ruch' + playerMove);
+  }
+  printMessage('Twój ruch: ' + playerMove);
+  if (playerInput == '3') {
+    playerMove = 'nożyce';
+  } else {
+    printMessage('nieznany ruch' + playerMove);
+  }
+  printMessage('Twój ruch: ' + playerMove);
